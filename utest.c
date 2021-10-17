00ad1a76e6b3f7f425ba540dc7b22c945bfe28fc
Trevor McKay <tm@trmckay.com>
2021-10-17 15:32:30 -0700

    variable output width for test names

--- utest.c
+++ utest.c
@@ -15,12 +15,15 @@ int __printstub(const char *format, ...) {
 int (*utest_printer)(const char *, ...) = __printstub;
 #endif
 
+static int tname_width = 18;
+
 test_t tests[UTEST_MAX_TESTS];
 static size_t test_count = 0;
 
 void register_test(int_fn_void fn, void_fn_void setup, void_fn_void teardown,
                    char *name) {
     test_t test;
+    size_t nlen;
 
     if (test_count == UTEST_MAX_TESTS) {
         utest_printer("Too many tests (max tests = %d)!\n", UTEST_MAX_TESTS);
@@ -32,20 +35,25 @@ void register_test(int_fn_void fn, void_fn_void setup, void_fn_void teardown,
     test.teardown = teardown;
     strncpy(test.name, name, UTEST_NAME_SIZE);
 
+    nlen = strlen(test.name);
+    if (nlen + 2 > tname_width) {
+        tname_width = nlen + 2;
+    }
+
     tests[test_count++] = test;
 }
 
 int run_tests(void) {
-    int status, failed = 0, passed = 0, nspace, nlen;
+    int status, failed = 0, passed = 0;
     test_t test;
-    size_t i, j;
+    size_t i, j, nspace, nlen;
 
     for (i = 0; i < test_count; i++) {
         test = tests[i];
 
         utest_printer(CYAN "%s" RESET, test.name);
         nlen = strlen(test.name);
-        nspace = (nlen >= 18) ? 1 : 18 - nlen;
+        nspace = (nlen >= tname_width) ? 1 : tname_width - nlen;
         for (j = 0; j < nspace; j++) {
             utest_printer(" ");
         }
